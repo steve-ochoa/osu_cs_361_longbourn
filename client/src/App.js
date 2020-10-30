68cb346eacb2297055fc1bd13ca1e2c3d3b379cc
gwond <gwond@oregonstate.edu>
2020-10-30 01:31:47 -0400

    Added components to landing page, vertically aligned elements, and updated links in NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import NavBar from "./components/Navbar";
 import "./App.css";
-import { Container } from "react-bootstrap";
+import { Container, Button, Row } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Container fluid="lg" style={{textAlign: "center"}}>
-        <h1>Expert Finderrr</h1>
+      <Container className="center" fluid="md" style={{ textAlign: "center", marginTop: "20%" }}>
+          <h1>Expert Finderrr</h1>
+          <p>Find contact information for experts in an area of knowledge or skill you specify. Share your areas of expertise with others.</p>
+          <Button variant="primary" size="lg" href="/Search">Find Your Expert</Button>
       </Container>
     </>
   );
