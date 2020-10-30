563d4b6142f94ea2be29066403b11bc008133be3
contip <contip@oregonstate.edu>
2020-10-30 15:35:37 -0400

    add routing, implement navbar links

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import NavBar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
+import RegForm from "./components/RegForm";
 import SearchPage from "./pages/SearchPage";
-import ExpertResultsPage from "./pages/ExpertResultsPage";
 import "./App.css";
-import { Container, Button, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ExpertResultsPage from "./pages/ExpertResultsPage";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Container className="center" fluid="md" style={{ textAlign: "center", marginTop: "20%" }}>
-          <h1>Expert Finderrr</h1>
-      </Container>
+      <Router>
+        <NavBar />
+        <Container
+          className="center"
+          fluid="md"
+          style={{ textAlign: "center", marginTop: "20%" }}
+        >
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/register" component={RegForm} />
+            <Route exact path="/results" component={ExpertResultsPage} />
+            <Route exact path="/search" component={SearchPage} />
+          </Switch>
+        </Container>
+      </Router>
     </>
   );
 }
