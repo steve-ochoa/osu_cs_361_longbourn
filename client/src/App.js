5b627a707957f56d7ad31d1b5d85e8eda6ef9229
contip <contip@oregonstate.edu>
2020-10-30 13:57:23 -0400

    fix conflicts with app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,14 @@ import LandingPage from "./pages/LandingPage";
 import SearchPage from "./pages/SearchPage";
 import ExpertResultsPage from "./pages/ExpertResultsPage";
 import "./App.css";
-<<<<<<< HEAD
-import { Container } from "react-bootstrap";
-import RegForm from "./components/RegForm";
-=======
 import { Container, Button, Row } from "react-bootstrap";
->>>>>>> master
 
 function App() {
   return (
     <>
       <NavBar />
-<<<<<<< HEAD
-      <Container
-        className="center"
-        fluid="md"
-        style={{ textAlign: "center", marginTop: "20%" }}
-      >
-        <h1>Expert Finderrr</h1>
-=======
       <Container className="center" fluid="md" style={{ textAlign: "center", marginTop: "20%" }}>
           <h1>Expert Finderrr</h1>
->>>>>>> master
       </Container>
     </>
   );
