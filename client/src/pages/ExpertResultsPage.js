b82ce5456fa55c9bb7431916e80d70226dee9318
gwond <gwond@oregonstate.edu>
2020-11-02 04:07:55 -0500

    Fetched expert data to populate cards on results page
    - experts dependent on skill/course/company searched
    - get skillIds(course/company) and compare with expertIds?

--- client/src/pages/ExpertResultsPage.js
+++ client/src/pages/ExpertResultsPage.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/ExpertCard";
 import { Container, } from "react-bootstrap";
+import { customFetch } from "../components/Helpers";
 
 
 function ExpertResultsPage() {
-    
-    return (
+  const [expertsData, setExpertData] = useState({
+    firstName: "",
+    description: "",
+    photoUrl: null,
+  });
+  const [contactData, setContactData] = useState({ linkedInUrl: "" });
+
+  useEffect(() => {
+    async function fetchData() {
+      const expertsData = await customFetch(
+        "http://flip3.engr.oregonstate.edu:6997/experts/"
+      );
+      console.log("Your experts are are: ", expertsData);
+      setExpertData(expertsData);
+    }
+    fetchData();
+  }, []);
+
+  // TODO: create key for each card
+  //       align cards 3/4 to a row
+
+  return (
+    Array.from(expertsData).map(expert => {
+      return (
         <Container className="center" fluid="md" style={{ textAlign: "center", marginTop: "20%" }}>
-            <Card />
+          <Card
+            name={expert.firstName}
+            description={expert.description}
+            photoUrl={expert.photoUrl}
+          />
         </Container>
-    );
+      )
+    }));
+
 }
 
 export default ExpertResultsPage;
