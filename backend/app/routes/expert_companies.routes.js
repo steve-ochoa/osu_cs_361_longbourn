c46d7827b5f9e977b7874a616af03af4f5e9b1fe
seokhchoi <choiseok@oregonstate.edu>
2020-11-02 12:40:37 -0500

    /expertCompanies
    Added
    1) expert_companies.route.js
    2) expert_companies.controller.js
    3) expert_companies.model.js

---
+++ backend/app/routes/expert_companies.routes.js
@@ -0,0 +1,15 @@
+// SHC notes: 
+// creates/retrieves expert_companies on DB
+// From Slack group discussion: (1) routes - define the route and have a callback method from one of the controllers
+// (code adapted from expert_skill.routes.js)
+
+
+module.exports = app => {
+	const expertCourse = require('../controllers/expert_companies.controller.js');
+
+	//Create new ExpertCompanies
+	app.post("/expertCompanies", expertCompanies.create);
+
+	//Retrieve ExpertCompanies by expertId
+	app.get("/expertCompanies/:expertId", expertCompanies.findByExpertId);
+}
