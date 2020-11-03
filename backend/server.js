3f94e4738d989d33d702e5969eed590757bc78f3
seokhchoi <choiseok@oregonstate.edu>
2020-11-02 19:54:35 -0500

    /findExperts/company
    endpoint for example /findExperts/company/1 to lookup all Experts who have a company with id=1

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ require('./app/routes/expert_companies.routes')(app);
 require('./app/routes/expert_course.routes')(app);
 require('./app/routes/findExpertsBySkill.routes')(app);
 require('./app/routes/findExpertsByCourse.routes')(app);
+require('./app/routes/findExpertsByCompany.routes')(app);
 
 // Start server
 const PORT = process.env.PORT || 6997;
