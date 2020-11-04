49f2e334c3461f77a4c069c3288159439dfb2d2a
Brian Rud <brud.91@gmail.com>
2020-11-03 20:39:09 -0500

    Add update functionality to Companies route

--- backend/app/controllers/company.controller.js
+++ backend/app/controllers/company.controller.js
@@ -40,3 +40,32 @@ exports.findAll = (req, res) => {
 		}
 	})
 }
+
+exports.update = (req, res) => {
+    //Validate request
+    if(!req.body)
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    
+    // Create a Company
+    const company = Company.fromReqBody(req.body);
+
+    // Update Company in the database
+    Company.updateById(company.companyId, company, (err, data) => {
+        if(err){
+            if (err.kind  === "not_found"){
+                res.status(404).send({
+                    message: `No Company with company_id ${company.company}`
+                });
+            }
+
+            else{
+                res.status(500).send({
+                    message: `Error updating Company with company_id ${company.companyId}`
+                });
+            };
+        } else res.send(data);
+               
+    });
+};
