2c05d9d3a37c1b18cf51af054989cd1e7870fc9f
Brian Rud <brud.91@gmail.com>
2020-11-03 20:17:50 -0500

    Add update functionality to courses route

--- backend/app/routes/course.routes.js
+++ backend/app/routes/course.routes.js
@@ -1,9 +1,12 @@
 module.exports = app => {
     const course = require('../controllers/course.controller.js');
 
-    //Create a new Skill
+    //Create a new Course
     app.post("/courses", course.create);
 
-    //Retrieve all Skills
+    //Retrieve all Courses
     app.get("/courses", course.findAll);
+
+    //Updade Course by CourseId
+    app.put("/courses/:courseId", course.update)
 };
