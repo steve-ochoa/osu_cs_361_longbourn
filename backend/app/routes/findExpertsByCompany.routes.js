e6d3f1e85e24d1af7143064bc00c548af407741e
seokhchoi <choiseok@oregonstate.edu>
2020-11-15 07:55:02 -0500

    removing files w/ duplicate code
    see previous commit

--- backend/app/routes/findExpertsByCompany.routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const expertsByCompany = require('../controllers/findExpertsByCompany.controller');
-
-    //Retrieve Expert By Company
-    app.get("/findExperts/company/:companyId", expertsByCompany.fetchExpertByCompanyId);
-
-};
