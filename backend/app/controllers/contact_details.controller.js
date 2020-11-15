026b8084baa765a423ffa61d11f179cc55fbab41
Brian Rud <brud.91@gmail.com>
2020-11-15 17:08:31 -0500

    Refactor contact_details from model/controller/route to dao/service/controller/route

--- backend/app/controllers/contact_details.controller.js
+++ backend/app/controllers/contact_details.controller.js
@@ -1,22 +1,6 @@
-const cd = require('../models/contact_details.model');
-const ContactDetails = cd.ContactDetails;
+const ContactDetailsServices = require('../services/contact_details_services')
 
 // Find ContactDetails for one Expert
 exports.findOneByExpertId = (req, res) => {
-    ContactDetails.fetchByExpertId(req.params.expertId, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `No ContactDetails with expert_id ${req.params.expertId}.`
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error retrieving ContactDetails with expert_id " + req.params.expertId
-                });
-            }
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(data)
-        }
-    });
+    ContactDetailsServices.findOneByExpertId(req,res);
 };
