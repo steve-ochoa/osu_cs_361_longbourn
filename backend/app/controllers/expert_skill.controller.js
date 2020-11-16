72ec84b236aee38c8fc16d78dfb58df39f212cf0
seokhchoi <choiseok@oregonstate.edu>
2020-11-16 09:11:43 -0500

    fixed smells on expert_skill files

--- backend/app/controllers/expert_skill.controller.js
+++ backend/app/controllers/expert_skill.controller.js
@@ -1,18 +1,44 @@
 const ExpertSkill = require('../services/expert_skill_service');
 
-//Create new ExpertCourse
+
+//Create new expert_skills
 exports.create = (req, res) => {
-	//Validate request
-	if(!req.body) {
-		res.status(400).send({
-			message:"Content cannot be empty!"
-		});
-	}
-	ExpertSkill.create(req, res);
+    ExpertSkill.create(req.body, (err, data) => {
+        if (err) {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while creating the expert_skills"
+			});
+		}
+		else if(!req.body){
+			res.status(404).send({
+				message:"Content cannot be empty!"
+			});
+		}
+        else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(data)
+        }
+    });
 
 }
 
+// Get Skill by ExpertId
 exports.fetchByExpertId = (req, res) => {
-	ExpertSkill.fetchByExpertId(req, res);	
+	ExpertSkill.fetchByExpertId(req.params.expertId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `No Skill with that expertId ${req.params.expertId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving Skill with such expertId " + req.params.expertId
+                });
+            }
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(data)
+        }
+    });
 }
-
--- backend/app/services/expert_skill_service.js
+++ backend/app/services/expert_skill_service.js
@@ -1,40 +1,15 @@
-const ExpertSkill = require('../models/expert_skill.model');
 const ExpertSkillDao = require('../daos/expert_skill_dao');
 
-exports.create = (req, res) => {
-    // Get req.body 
-    const expertSkill = req.body;    
-
-     // Send expertSkill (req.body) to ExpertSkillDao.create 
-    ExpertSkillDao.create(expertSkill, (err, newExpertSkill) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the expert_skills"
-            });
-        else 
-        {
-            res.send(newExpertSkill);
-        }
-    });
+/*
+Create expert_skills
+*/
+exports.create = (expertSkill, res) => {
+    ExpertSkillDao.create(expertSkill, res);
 };
 
-
-exports.fetchByExpertId = (req, res) => {
-    ExpertSkillDao.fetchByExpertId(req.params.expertId, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `No Expert with id ${req.params.expertId}.`
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error retrieving Expert with id " + req.params.expertId
-                });
-            }
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(data)
-        }
-    });
+/*
+Find Skill(s) by ExpertId
+*/
+exports.fetchByExpertId = (expertId, res) => {
+    ExpertSkillDao.fetchByExpertId(expertId, res);
 };
