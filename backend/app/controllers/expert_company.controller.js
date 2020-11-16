9c61b27ccb040495f0d444e8e2eb4fdbf89c987c
seokhchoi <choiseok@oregonstate.edu>
2020-11-16 09:03:43 -0500

    smell fixes on expert_company files

--- backend/app/controllers/expert_company.controller.js
+++ backend/app/controllers/expert_company.controller.js
@@ -2,16 +2,45 @@ const ExpertCompany = require('../services/expert_company_service')
 
 //Create new ExpertCompany
 exports.create = (req, res) => {
-	//Validate request
-	if(!req.body) {
-		res.status(400).send({
-			message:"Content cannot be empty!"
-		});
-	}
-	ExpertCompany.create(req, res);
+    ExpertCompany.create(req.body, (err, data) => {
+        if (err) {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while creating the expert_companies"
+			});
+		}
+		else if(!req.body){
+			res.status(404).send({
+				message:"Content cannot be empty!"
+			});
+		}
+        else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(data)
+        }
+    });
 
 }
 
+
+// Get Company by ExpertId
 exports.fetchByExpertId = (req, res) => {
-	ExpertCompany.fetchByExpertId(req, res);	
+	ExpertCompany.fetchByExpertId(req.params.expertId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `No Company with that expertId ${req.params.expertId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving Company with such expertId " + req.params.expertId
+                });
+            }
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(data)
+        }
+    });
 }
+
+
--- backend/app/services/expert_company_service.js
+++ backend/app/services/expert_company_service.js
@@ -1,42 +1,15 @@
-const ExpertCompany = require('../models/expert_company.model');
 const ExpertCompanyDao = require('../daos/expert_company_dao');
 
-
-exports.create = (req, res) => {
-    // Get req.body 
-    const expertCompany = req.body;    
-
-     // Send expertCompany (req.body) to ExpertCompanyDao.create 
-    ExpertCompanyDao.create(expertCompany, (err, newExpertCompany) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the expert_companies"
-            });
-        else 
-        {
-            // console.log(newExpertCompany);            
-            res.send(newExpertCompany);
-        }
-    });
+/*
+Create expert_companies
+*/
+exports.create = (expertCompany, res) => {
+    ExpertCompanyDao.create(expertCompany, res);
 };
 
-
-exports.fetchByExpertId = (req, res) => {
-    ExpertCompanyDao.fetchByExpertId(req.params.expertId, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `No Expert with id ${req.params.expertId}.`
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error retrieving Expert with id " + req.params.expertId
-                });
-            }
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(data)
-        }
-    });
+/*
+Find Company/Companies by ExpertId
+*/
+exports.fetchByExpertId = (expertId, res) => {
+    ExpertCompanyDao.fetchByExpertId(expertId, res);
 };
