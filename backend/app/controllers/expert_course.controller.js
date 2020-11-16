8e3fb97b8e7970a1a7ff412220488f496c699c08
seokhchoi <choiseok@oregonstate.edu>
2020-11-16 08:52:40 -0500

    Smell fixed on service of expert_course

--- backend/app/controllers/expert_course.controller.js
+++ backend/app/controllers/expert_course.controller.js
@@ -2,18 +2,46 @@ const ExpertCourse = require('../services/expert_course_service');
 
 //Create new ExpertCourse
 exports.create = (req, res) => {
-	//Validate request
-	if(!req.body) {
-		res.status(400).send({
-			message:"Content cannot be empty!"
-		});
-	}
-	ExpertCourse.create(req, res);
+    ExpertCourse.create(req.body, (err, data) => {
+        if (err) {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while creating the expert_courses"
+			});
+		}
+		else if(!req.body){
+			res.status(404).send({
+				message:"Content cannot be empty!"
+			});
+		}
+        else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(data)
+        }
+    });
 
 }
 
+
+
+// Get Course by ExpertId
 exports.fetchByExpertId = (req, res) => {
-	ExpertCourse.fetchByExpertId(req, res);	
+	ExpertCourse.fetchByExpertId(req.params.expertId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `No Experts with that expertId ${req.params.expertId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving Expert with such expertId " + req.params.expertId
+                });
+            }
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(data)
+        }
+    });
 }
 
 
--- backend/app/services/expert_course_service.js
+++ backend/app/services/expert_course_service.js
@@ -1,42 +1,15 @@
-const ExpertCourse = require('../models/expert_course.model');
 const ExpertCourseDao = require('../daos/expert_course_dao');
 
-
-exports.create = (req, res) => {
-    // Get req.body 
-    const expertCourse = req.body;    
-
-     // Send expertCourse (req.body) to ExpertCourseDao.create 
-    ExpertCourseDao.create(expertCourse, (err, newExpertCourse) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the expert_courses"
-            });
-        else 
-        {
-            // console.log(newExpertCourse);            
-            res.send(newExpertCourse);
-        }
-    });
+/*
+Create Course
+*/
+exports.create = (expertCourse, res) => {
+    ExpertCourseDao.create(expertCourse, res);
 };
 
-
-exports.fetchByExpertId = (req, res) => {
-    ExpertCourseDao.fetchByExpertId(req.params.expertId, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `No Expert with id ${req.params.expertId}.`
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error retrieving Expert with id " + req.params.expertId
-                });
-            }
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(data)
-        }
-    });
+/*
+Find Courses(s) by ExpertId
+*/
+exports.fetchByExpertId = (expertId, res) => {
+    ExpertCourseDao.fetchByExpertId(expertId, res);
 };
