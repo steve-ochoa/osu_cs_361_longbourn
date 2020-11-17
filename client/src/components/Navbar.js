cc7d67faf99cb698b8e73f7abd8d5f35a480e2dc
gwond <gwond@oregonstate.edu>
2020-11-17 10:53:16 -0500

    Added font color to navbar
    not pretty

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,17 +4,17 @@ import { Navbar, Nav } from "react-bootstrap";
 export default function NavBar() {
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">Expert Finder</Navbar.Brand>
+      <Navbar expand="lg">
+        <Navbar.Brand style={{ color: "#f6f5f5" }} href="/">Expert Finder</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/Search">Search</Nav.Link>
+            <Nav.Link style={{ color: "#f6f5f5" }} href="/">Home</Nav.Link>
+            <Nav.Link style={{ color: "#f6f5f5" }} href="/Search">Search</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="/register1">Register A Friend</Nav.Link>
-            <Nav.Link href="/register1">Register As An Expert</Nav.Link>
+            <Nav.Link style={{ color: "#f6f5f5" }} href="/register1">Register A Friend</Nav.Link>
+            <Nav.Link style={{ color: "#f6f5f5" }} href="/register1">Register As An Expert</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
