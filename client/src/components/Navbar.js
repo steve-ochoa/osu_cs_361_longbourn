c21cf73fd47fc830bad3cd4419a5a1e6d9d5462d
seokhchoi <choiseok@oregonstate.edu>
2020-11-28 10:02:13 -0500

    Navigation Bar Provides Pill Feedback when clicked on
    Links on navigation bar changes color (pill) when clicked on by user, letting user know that user clicked on it.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,13 +8,13 @@ export default function NavBar() {
         <Navbar.Brand style={{ color: "#f6f5f5" }} href="/">Expert Finder</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link style={{ color: "#f6f5f5" }} href="/">Home</Nav.Link>
-            <Nav.Link style={{ color: "#f6f5f5" }} href="/Search">Search</Nav.Link>
+          <Nav variant="pills" className="mr-auto">
+            <Nav.Link eventKey="1" style={{ color: "#f6f5f5" }} href="/">Home</Nav.Link>
+            <Nav.Link eventKey="2" style={{ color: "#f6f5f5" }} href="/Search">Search</Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link style={{ color: "#f6f5f5" }} href="/register1">Register A Friend</Nav.Link>
-            <Nav.Link style={{ color: "#f6f5f5" }} href="/register1">Register As An Expert</Nav.Link>
+          <Nav variant="pills">
+            <Nav.Link eventKey="3" style={{ color: "#f6f5f5" }} href="/register1">Register A Friend</Nav.Link>
+            <Nav.Link eventKey="4" style={{ color: "#f6f5f5" }} href="/register1">Register As An Expert</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
