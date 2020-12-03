0c34957c555c450bede260e533b7ec2c3652bec3
contip <contip@oregonstate.edu>
2020-12-02 23:16:41 -0500

    make github popup persistent until user clicks away

--- client/src/components/GitHubPopUp.js
+++ client/src/components/GitHubPopUp.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { OverlayTrigger, Popover, Button } from "react-bootstrap";
+import React, { useState, useEffect, useRef } from "react";
+import { Overlay, Popover, Button } from "react-bootstrap";
 import GitHubFetch from "./GitHubFetch";
 import GitHubListDisplay from "./GitHubListDisplay";
 
-/* TODO: make popup scrollable by targeting popover-body with css and adding a 
+/* TODO: make popup scrollable by targeting popover-body with css and adding a
  * height and "overflow-y: auto" */
 export default function GitHubPopUp({ userName }) {
   const [projectData, setProjectData] = useState([]);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const target = useRef(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -17,22 +19,33 @@ export default function GitHubPopUp({ userName }) {
 
   return (
     <>
-      <OverlayTrigger
+      <Button
+        ref={target}
+        onMouseOver={() => {
+          setShowOverlay(!showOverlay);
+        }}
+        variant="link"
+      >{`Github: ${userName}`}</Button>
+      <Overlay
+        show={showOverlay}
+        rootClose={true}
+        target={target.current}
+        onHide={() => {
+          setShowOverlay(false);
+        }}
         placement="right"
-        delay={{ show: 250, hide: 2500 }}
-        overlay={
-          <Popover style={{maxHeight: "500px", overflowY: "scroll"}}>
-            <Popover.Title as="h3">{`GitHub Projects`}</Popover.Title>
-            <Popover.Content>
-              {projectData.length > 0 && (
-                <GitHubListDisplay projects={projectData}/>
-              )}
-            </Popover.Content>
-          </Popover>
-        }
-      >
-        <Button variant="link">{`Github: ${userName}`}</Button>
-      </OverlayTrigger>
+      >{
+        <Popover
+          style={{ maxHeight: "500px", overflowY: "scroll" }}
+        >
+          <Popover.Title as="h3">{`GitHub Projects`}</Popover.Title>
+          <Popover.Content>
+            {projectData.length > 0 && (
+              <GitHubListDisplay projects={projectData} />
+            )}
+          </Popover.Content>
+        </Popover>}
+      </Overlay>
     </>
   );
 }
